3a04912ca75d09c578f173ae3bceb7d1fa489189
Tamal Saha <tamal@appscode.com>
2025-02-24 22:36:40 -0800

    Update build process
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- cmd/wal-listener/init.go
+++ cmd/wal-listener/init.go
@@ -1,3 +1,19 @@
+/*
+Copyright AppsCode Inc. and Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package main
 
 import (
@@ -5,11 +21,11 @@ import (
 	"fmt"
 	"log/slog"
 
-	"github.com/jackc/pgx"
-	"github.com/nats-io/nats.go"
-
 	"github.com/ihippik/wal-listener/v2/internal/config"
 	"github.com/ihippik/wal-listener/v2/internal/publisher"
+
+	"github.com/jackc/pgx"
+	"github.com/nats-io/nats.go"
 )
 
 // initPgxConnections initialise db and replication connections.
